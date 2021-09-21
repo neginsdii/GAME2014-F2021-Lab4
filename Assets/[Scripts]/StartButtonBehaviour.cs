3202b1348c7c111d17b894ed408a72e22c03798f
Negin <neginsaeidi@yahoo.com>
2021-09-21 14:59:12 -0400

    Added Start Scene, Main Scene, End Scene

--- Assets/[Scripts]/StartButtonBehaviour.cs
+++ Assets/[Scripts]/StartButtonBehaviour.cs
@@ -1,11 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class StartButtonBehaviour : MonoBehaviour
 {
     public void OnStartButtonPressed()
     {
-        Debug.Log("Start Button Pressed!");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
     }
 }
